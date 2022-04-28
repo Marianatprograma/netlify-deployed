bc01ad9b6711d5032d88f0489a1e00685f38abce
Marianatprograma <maritaborue@gmail.com>
2022-04-28 07:01:49 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Soy lo que subirás</h1>
+        <h2> Comprobando que suba cambios también</h2>
       </header>
     </div>
   );
